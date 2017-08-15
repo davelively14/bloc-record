8e2bd8236e59388dba687ea20af941e241615dc2
Dave Lively <davelively14@gmail.com>
2017-08-15 19:02:33 -0400

    Added instance_variables_to_hash and docs for other methods

--- lib/bloc_record/utility.rb
+++ lib/bloc_record/utility.rb
@@ -2,6 +2,12 @@ module BlocRecord
   module Utility
     extend self
 
+    # Converts a CamelCaseWord to a a snake_case_word.
+    # ex:
+    # underscore("BFGoodrich")
+    # "bf_goodrich"
+    # underscore("UsersOftenDoItWrong")
+    # "users_often_do_it_wrong"
     def underscore(camel_case_word)
       string = camel_case_word.gsub(/::/, '/')
       string.gsub!(/([A-Z]+)([A-Z][a-z])/,'\1_\2')
@@ -10,6 +16,10 @@ module BlocRecord
       string.downcase
     end
 
+    # Turns strings and numerics into SQL strings.
+    # ex:
+    # puts sql_strings("hello")
+    # "'hello'"
     def sql_strings(value)
       case value
       when String
@@ -21,9 +31,18 @@ module BlocRecord
       end
     end
 
-    def convert_string(options)
+    # Will convert all symbol keys to string keys. In other words, it'll take
+    # {this: "that"} and convert it to {"this" => "that"}.
+    def convert_keys(options)
       options.keys.each { |k| options[k.to_s] = options.delete(k) if k.kind_of?(Symbol) }
       options
+    end
+
+    def instance_variables_to_hash(obj)
+      # instance_variables returns an array of variable names as atoms (i.e.
+      # [:@time, @date]). The map will return [["var_name", var_value]...].
+      # Hash will turn that into a hash: {"var_name" => var_value, ...}
+      Hash[obj.instance_variables.map{ |var| ["#{var.to_s.delete('@')}", obj.instance_variable_get(var.to_s)] }]
     end
   end
 end
